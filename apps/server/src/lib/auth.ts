7edc2e50a6d5f53c6fef3b473485b568035e3362
Cillian Barron <barroncillian@gmail.com>
2025-04-11 19:56:24 +0100

    feat: add organisation client

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,13 +1,14 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { organization } from "better-auth/plugins";
 import { db } from "../db";
 import * as schema from "../db/schema/auth";
-
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: "pg",
 		schema: schema,
 	}),
+	plugins: [organization()],
 	trustedOrigins: [process.env.CORS_ORIGIN || ""],
 	emailAndPassword: {
 		enabled: true,
